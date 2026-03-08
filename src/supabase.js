c95b1630a063e2e957ab43b55981ef1d2220bccb
carranza-dev <carranzaisaac243@gmail.comgit config --global --listgit config -globalgit config --global user.email carranzaisaac243@gmail.com>
2026-03-08 15:19:45 -0600

    fix: add supabase credentials for production

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+const supabaseUrl = 'https://yhrrvrlgpamprlyrdbpo.supabase.co'
+const supabaseKey = 'sb_publishable_-FPuUzXJ7uMJekKWtOZzIQ_3YOotsT-'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
